451b57de2ddb1decebab62a76617d63e6269c7d4
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-04-03 14:46:00 +0000

    Add images in releases page

--- db/releases/queries/read.sql
+++ db/releases/queries/read.sql
@@ -3,7 +3,8 @@ SELECT
     g.name AS name,
     p.name AS platform,
     e.name AS edition,
-    re.name AS region
+    re.name AS region,
+    r.cover_image_path AS "coverImagePath"
 FROM releases AS r
 JOIN games AS g ON g.id = r.game_id
 JOIN platforms AS p ON p.id = r.platform_id 
